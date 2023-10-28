5f7b38d90fd78f97305b28847f8b82f359f009a6
nedaei79 <nedaei79@gmail.com>
2023-10-28 22:32:56 +0330

    add the main structure of the project

---
+++ main.py
@@ -0,0 +1,13 @@
+def get_fibonacci(n: int) -> int:
+  return 0
+
+
+def print_result(fibonacci_n: int):
+  print(fibonacci_n)
+
+
+n = int(input())
+
+fibonacci_n = get_fibonacci(n)
+
+print_result(fibonacci_n)
