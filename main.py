345428aa093d02722490dca11a3ca6c6ecb627ce
AliMehrabani <ali1379m@gmail.com>
2023-10-29 19:42:32 +0330

    Implement the new print_result method

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 def get_fibonacci(n: int) -> list:
-  return [1]
+  return [1, 1, 2]
 
 
 def print_result(fibonacci_n: list):
-  print(fibonacci_n)
+  for i in range(len(fibonacci_n) - 2):
+    print(f"f[{i}]: {fibonacci_n[i]}  +  f[{i+1}]: {fibonacci_n[i+1]}  ->  f[{i+2}]: {fibonacci_n[i+2]}")
 
 
 n = int(input())
