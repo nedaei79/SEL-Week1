8cec8cca9cce9c79afe771e08affce1e1da5b22f
nedaei79 <nedaei79@gmail.com>
2023-10-29 21:47:21 +0330

    edit function definition order

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
+def print_result(fibonacci_n: list):
+  for i in range(len(fibonacci_n) - 2):
+    print(f"f[{i}]: {fibonacci_n[i]}  +  f[{i+1}]: {fibonacci_n[i+1]}  ->  f[{i+2}]: {fibonacci_n[i+2]}")
+
+
 def get_fibonacci(n: int) -> list:
   res = [1, 1]
   for i in range(n - 1):
     res.append(res[-1] + res[-2])
   return res[:n + 1]
-
-
-def print_result(fibonacci_n: list):
-  for i in range(len(fibonacci_n) - 2):
-    print(f"f[{i}]: {fibonacci_n[i]}  +  f[{i+1}]: {fibonacci_n[i+1]}  ->  f[{i+2}]: {fibonacci_n[i+2]}")
-
 
 n = int(input())
 
