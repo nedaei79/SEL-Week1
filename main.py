6434f361cf2f105c9b4d9d70b58c89ed569f22ed
AliMehrabani <ali1379m@gmail.com>
2023-10-29 21:48:26 +0330

    add a comment in print result method

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def get_fibonacci(n: int) -> list:
 
 
 def print_result(fibonacci_n: list):
+  # print every sum, done in get_fibonacci
   for i in range(len(fibonacci_n) - 2):
     print(f"f[{i}]: {fibonacci_n[i]}  +  f[{i+1}]: {fibonacci_n[i+1]}  ->  f[{i+2}]: {fibonacci_n[i+2]}")
 
